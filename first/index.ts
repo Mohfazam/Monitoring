511ebf1b4842bdebb0f327120765474751676418
Mohfazam <mohd.sarwar.code@gmail.com>
2026-05-19 20:17:12 +0530

    Update

--- first/index.ts
+++ first/index.ts
@@ -3,13 +3,19 @@ import express  from "express";
 const app = express();
 
 app.get("/cpu", (req, res) => {
+    const startTime = Date.now();
     for(let i = 0; i < 1000000; i++){
         Math.random();
     }
 
+    
+
     res.json({
         message: "cpu"
     });
+    const endTime = Date.now();
+
+    console.log(`Time it took ${endTime - startTime}ms`);
 });
 
 app.get("/users", (req, res) => {
