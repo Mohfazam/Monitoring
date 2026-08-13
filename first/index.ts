9509144f2c65012ef43238d54c16e79cabd884b0
Mohfazam <mohd.sarwar.code@gmail.com>
2026-08-14 03:38:18 +0530

    counter increment hook call

--- first/index.ts
+++ first/index.ts
@@ -25,6 +25,23 @@ const requestsCounter = new client.Counter({
     labelNames: ['method', 'route', 'status_code']
 });
 
+const requestCountMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const startTime = Date.now();
+
+    res.on('finish', () => {
+        const endTime = Date.now();
+        console.log(`Request took ${endTime - startTime}ms`);
+
+        requestsCounter.inc({
+            method: req.method,
+            route: req.route ? req.route.path: req.path,
+            status_code: res.statusCode
+        });
+    });
+
+    next();
+};
+
 
 const app = express();
 app.use(middleWare);
